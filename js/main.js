06d0cc33b60db5e632e594322d7a172119659669
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-09-01 16:08:56 +0200

    add squareGenerator function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-var field = document.getElementById('field');
-
-for (var i = 1; i <= 100; i++){
-  field.innerHTML += `<div class="square">${i}</div>`
+/* squareGenerator genera un certo numero di quadrati 
+*  @param num numero di quadrati da generare 
+*/
+function squareGenerator(num) {
+  var field = document.getElementById('field');
+  
+  for (var i = 1; i <= num; i++){
+    field.innerHTML += `<div class="square">${i}</div>`
+  }
+  
 }
 
+// chiedo all'utente il numero di quadrati da inserire
+var squareNumber = prompt('inserisci il numero di quadrati');
+
+// genero i quadrati
+squareGenerator(squareNumber)
+
+// al clik il quadrato diventa rosso ed appare un alert() con il numero del quadrato selezionato
 field.addEventListener('click',
-  function (e) {
-    e.target.classList.toggle('clicked')
-  }
+function (e) {
+  e.target.classList.add('clicked')
+  alert(e.target.innerHTML)
+ }
 )
